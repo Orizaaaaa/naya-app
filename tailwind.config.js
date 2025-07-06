7acedf89c19b54fc8a930e26ade3b33d2566ba3e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-06 15:23:19 +0700

    fiish

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/**/*.{js,ts,jsx,tsx,mdx}",
-        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
 
     plugins: [heroui(),],
