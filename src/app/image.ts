c8dc39a6e2c3fe47d775ce2264f3d5c5a67f2b6c
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-11-11 20:20:33 +0700

    finish all

--- src/app/image.ts
+++ src/app/image.ts
@@ -1,7 +1,7 @@
 import oneLogo from '../assets/oneLogo.svg'
 import human from '../assets/human.jpg'
 import logo from '../assets/logo.svg'
-import realLogo from '../assets/realLogo.svg'
+import realLogo from '../assets/logo_new.svg'
 import warning from '../assets/warning.png'
 
 export { oneLogo, logo, human, realLogo, warning }
